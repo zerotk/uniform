95623cc692a1ba920377636838a272f5cd43b010
Alexandre Andrade <kaniabi@gmail.com>
2016-03-20 22:46:25 -0300

    Support for NO_REWRITE_FILENAMES.

--- zerotk/uniform/__init__.py
+++ zerotk/uniform/__init__.py
@@ -4,13 +4,23 @@ from zerotk.easyfs import FindFiles, GetFileContents, CreateFile, EOL_STYLE_UNIX
 import yaml
 
 
+NO_REWRITE_FILENAMES = (
+    'README.md',
+)
+
 def create_project(project_name, config_filename):
 
     config = yaml.load(open(config_filename))
 
     template_dir = os.path.dirname(__file__) + '/template'
     for i_template_filename in FindFiles(template_dir, '*.*'):
-        target_filename = project_name + '/' + os.path.basename(i_template_filename)
+        filename = os.path.basename(i_template_filename)
+        target_filename = project_name + '/' + filename
+
+        if filename in NO_REWRITE_FILENAMES:
+            if os.path.isfile(target_filename):
+                continue
+
         create_project_file(
             target_filename,
             i_template_filename,
